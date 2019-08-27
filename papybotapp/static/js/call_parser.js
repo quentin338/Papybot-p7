268817d656677ef74c2d82325a593b427b60ac09
quentin338 <quentin.bertrand@yahoo.fr>
2019-08-28 01:04:25 +0200

    Began new template. Trying something a easier with full bootstrap rows and cols. Fixed GMaps to not being displayed at the beginning or when no location has been found.

--- papybotapp/static/js/call_parser.js
+++ papybotapp/static/js/call_parser.js
@@ -17,9 +17,12 @@ function ajaxPost(url, data, callback) {
 
 
 function initMap(lat, lng) {
+    let needToDisplayMap = true;
+
     if (lat === undefined || lng === undefined) {
         lat = 0;
         lng = 0;
+        needToDisplayMap = false;
     }
 
     let myLatLng = {lat: lat, lng: lng};
@@ -34,100 +37,17 @@ function initMap(lat, lng) {
       map: map,
       title: 'Ici mon petit !'
     });
+
+    let mapHide = $('#map');
+    // If we need to display the map, we always remove .map-hide, else we always add it.
+    if (needToDisplayMap === false) {
+        mapHide.toggleClass("map-hide", true);
+    } else {
+        mapHide.toggleClass("map-hide", false);
+    }
   }
 
 let form = document.querySelector("form");
-
-let firstMapDisplay = true;
-
-// form.addEventListener("submit", function (e) {
-//     e.preventDefault();
-//
-//     // if (firstMapDisplay === true) {
-//     // document.getElementById("map").innerHTML = "";
-//     // }
-//
-//     // Creating FormData and we pass it the input text to send it to the view
-//     let form = new FormData();
-//     form.append("query", e.target.elements.input.value);
-//
-//     ajaxPost("/parser/", form, function (response) {
-//         response = JSON.parse(response);
-//
-//         let displayElt = document.getElementById("display-result");
-//
-//         // We remove the first displayed map as we create one for each result
-//         if (firstMapDisplay === true) {
-//             let mapElt = document.getElementById("map");
-//             mapElt.parentNode.removeChild(mapElt);
-//             firstMapDisplay = false;
-//         }
-//
-//         // Creating a div with Papybot's image
-//         let papybotDivElt = document.createElement("div");
-//         papybotDivElt.id = "papybot-result-img";
-//         papybotDivElt.classList.add("papybot-result-img");
-//
-//         let papybotImgElt = document.createElement("img");
-//         papybotImgElt.src = "static/img/papybot300x300.jpg";
-//         papybotImgElt.alt = "Papybot";
-//
-//         papybotDivElt.appendChild(papybotImgElt);
-//
-//         // Creating a div who'll have 3 <p> elts : content, url and Papybot's answer
-//         let resultsElt = document.createElement("div");
-//         resultsElt.classList.add("results");
-//
-//         // Inserting the last search on top
-//         displayElt.insertBefore(resultsElt, displayElt.childNodes[0]);
-//
-//         // Creating div for result text content
-//         let textContentDiv = document.createElement("div");
-//         textContentDiv.id = "text-content";
-//         textContentDiv.classList.add("text-content");
-//
-//         // Content <p>
-//         let contentPElt = document.createElement("p");
-//         contentPElt.textContent = response.content;
-//         contentPElt.classList.add("wiki-content");
-//
-//         // Url <p>
-//         let urlPElt = document.createElement("p");
-//         let aElt = document.createElement("a");
-//         aElt.setAttribute("href", response.url);
-//         aElt.textContent = "Pour en savoir plus";
-//
-//         urlPElt.appendChild(aElt);
-//
-//         // PapyBot's answer <p>
-//         let papybotPElt = document.createElement("p");
-//         papybotPElt.textContent = "PapyBot : " + response.bot_response;
-//
-//         // Adding all 3 <p> to the div
-//         textContentDiv.appendChild(papybotPElt);
-//         textContentDiv.appendChild(contentPElt);
-//         textContentDiv.appendChild(urlPElt);
-//
-//         // Adding the div the result div
-//         resultsElt.appendChild(papybotDivElt);
-//         resultsElt.append(textContentDiv);
-//
-//         console.log(response);
-//
-//         // Creating new Map
-//         let newMapElt = document.createElement("div");
-//         newMapElt.id = "map";
-//         newMapElt.classList.add("map");
-//
-//         resultsElt.appendChild(newMapElt);
-//
-//         // Display Google Map
-//         initMap(response.coords[0], response.coords[1]);
-//
-//         // Removing id to be able to keep it while creating other maps
-//         newMapElt.removeAttribute("id");
-//     })
-// });
 
 let lastSearch = undefined;
 
