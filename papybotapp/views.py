d506733672e1489672e6f6a729522718d303501b
quentin338 <quentin.bertrand@yahoo.fr>
2019-09-05 18:31:59 +0200

    Moved /parser test results before going prod.

--- papybotapp/views.py
+++ papybotapp/views.py
@@ -15,18 +15,6 @@ def index():
 @app.route('/parser', methods=["POST"])
 def parser():
     if request.method == "POST":
-        results = {
-            'address': "10, rue des artistes",
-            'coords': (10, 10),
-            'url': "https://www.whatismyip.com",
-            'content': "It was the first structure"
-                       "ial at the top of the tower in 1957,"
-                       "ng transmitters, the Eiffel Tower is "
-                       "ct.",
-            'thumbnail': "/static/img/wiki_logo.jpg",
-            'bot_response': "La réponse de papybot !"
-        }
-
         results = main_func(str(request.data))
 
         return jsonify(results)
@@ -34,3 +22,14 @@ def parser():
 
 if __name__ == "__main__":
     app.run()
+    # results = {
+    #     'address': "10, rue des artistes",
+    #     'coords': (10, 10),
+    #     'url': "https://www.whatismyip.com",
+    #     'content': "It was the first structure"
+    #                "ial at the top of the tower in 1957,"
+    #                "ng transmitters, the Eiffel Tower is "
+    #                "ct.",
+    #     'thumbnail': "/static/img/wiki_logo.jpg",
+    #     'bot_response': "La réponse de papybot !"
+    # }
